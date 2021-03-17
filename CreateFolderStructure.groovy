b1a26c9c833cb98a35f40e192fdb0e383923cc17
Alexander <alex.chaly13@gmail.com>
2021-03-17 11:30:00 +0200

    vcx

--- CreateFolderStructure.groovy
+++ CreateFolderStructure.groovy
@@ -1,8 +1,98 @@
-folder('project-a') {
-    displayName('Project A')
-    description('Folder for project A')
+folder('APIGEE') {
+    displayName('APIGEE')
+    description('Folder for project APIGEE')
 }
 
-folder('project-a/testing') {
-    description('Folder containing all QA jobs for project A')
+folder('APIGEE/NON-PROD') {
+    description('Folder containing non-prod jobs for Apigee')
+}
+
+folder('APIGEE/NON-PROD/proxies') {
+    description('Folder containing non-prod jobs for Apigee')
+}
+
+folder('APIGEE/NON-PROD/proxies/v1') {
+    description('Folder containing v1 jobs for Apigee')
+}
+
+folder('APIGEE/NON-PROD/proxies/v1/ecom-branches-proxy') {
+    description('Folder containing ecom-branches-proxy job')
+}
+
+folder('APIGEE/NON-PROD/proxies/v1/ecom-branches-proxy/dev') {
+    description('Folder containing ecom-branches-proxy job on dev env')
+}
+
+folder('APIGEE/NON-PROD/proxies/v1/ecom-branches-proxy/pre-prod') {
+    description('Folder containing ecom-branches-proxy job on pre-prod env')
+}
+
+folder('APIGEE/NON-PROD/proxies/v1/ecom-branches-proxy/qa') {
+    description('Folder containing ecom-branches-proxy job on qa env')
+}
+
+folder('APIGEE/NON-PROD/shared-flows') {
+    description('Folder containing non-prod jobs for Apigee')
+}
+
+folder('APIGEE/NON-PROD/shared-flows/v1') {
+    description('Folder containing non-prod shared flows for Apigee')
+}
+
+folder('APIGEE/NON-PROD/shared-flows/v1/default-fault') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/default-fault/dev') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/default-fault/pre-prod') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/default-fault/qa') {
+    description('Folder containing shared flow')
+}
+
+folder('APIGEE/NON-PROD/shared-flows/v1/proxy-fault') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/proxy-fault/dev') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/proxy-fault/pre-prod') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/proxy-fault/qa') {
+    description('Folder containing shared flow')
+}
+
+folder('APIGEE/NON-PROD/shared-flows/v1/target-fault') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/target-fault/dev') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/target-fault/pre-prod') {
+    description('Folder containing shared flow')
+}
+folder('APIGEE/NON-PROD/shared-flows/v1/target-fault/qa') {
+    description('Folder containing shared flow')
+}
+
+folder('APIGEE/PROD') {
+    description('Folder containing prod jobs for Apigee')
+}
+
+folder('APIGEE/PROD/proxies') {
+    description('Folder containing prod jobs for Apigee')
+}
+folder('APIGEE/PROD/proxies/v1') {
+    description('Folder containing v1 jobs')
+}
+
+folder('APIGEE/PROD/shared-flows') {
+    description('Folder containing prod jobs for Apigee')
+}
+
+folder('APIGEE/PROD/shared-flows/v1') {
+    description('Folder containing v1 jobs')
 }
